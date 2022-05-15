a51bc2320f0a0227fb04b5e617b50374482a5c03
yjjung <yjjung@lifegoeson.kr>
2022-05-15 15:37:53 +0900

    :sparkles: annotation container

--- java8to11/src/java8to11/App.java
+++ java8to11/src/java8to11/App.java
@@ -1,24 +1,23 @@
 package java8to11;
 
 import java.util.Arrays;
-import java.util.List;
 
 /**
  * @author yjjung
  * @version 0.1.0
  * @since 2022/04/25
  */
-@Chicken
+@Chicken("양념")
+@Chicken("간장")
 public class App {
 
-    public static void main(@Chicken String[] args) throws @Chicken RuntimeException {
-        List<@Chicken String> names = Arrays.asList("yj");
+    public static void main( String[] args) {
+
+        final var chickens = App.class.getAnnotationsByType(Chicken.class);
+        Arrays.stream(chickens).forEach(c -> System.out.println("c = " + c.value()));
+
+        final var chickenContainer = App.class.getAnnotation(ChickenContainer.class);
+        Arrays.stream(chickenContainer.value()).forEach(c -> System.out.println("cc = " + c.value()));
     }
 
-    static class FeelsLikeChicken<@Chicken T> {
-
-        public static <@Chicken C> void print(@Chicken C c) {
-            System.out.println(c);
-        }
-    }
 }
--- java8to11/src/java8to11/Chicken.java
+++ java8to11/src/java8to11/Chicken.java
@@ -1,9 +1,6 @@
 package java8to11;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+import java.lang.annotation.*;
 
 /**
  * @author yjjung
@@ -12,6 +9,7 @@ import java.lang.annotation.Target;
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE_USE)
+@Repeatable(ChickenContainer.class)
 public @interface Chicken {
-
+    String value();
 }
