fd5b488e9057c74a51ec4c31e2583991523f3942
treasureBear94 <qvo7896@gmail.com>
2022-09-11 17:13:06 +0900

    ✨ 자바 알고리즘 03-04 성공 - O(n^2)이기 때문에 O(n) 나올 수 있도록 재시도

--- algorithm/src/inflearnjavaalgo/p0304/Main.java
+++ algorithm/src/inflearnjavaalgo/p0304/Main.java
@@ -9,7 +9,7 @@ class Main {
         int answer = 0;
         for (int i = 0; i < n; i++) {
             int sum = 0;
-            for (int j = i; j < n; j++) {
+            for (int j = i; j < n; j++) {   // O(n^2)
                 sum += arr[j];
                 if (sum == m) {
                     answer++;
