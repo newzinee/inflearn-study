36be9dff84a308632399bcc779923ce61b477aef
newzinee <qvo7896@gmail.com>
2023-03-26 12:43:39 +0900

    ✨ log aop

--- aop/src/test/java/hello/aop/exam/ExamTest.java
+++ aop/src/test/java/hello/aop/exam/ExamTest.java
@@ -1,9 +1,11 @@
 package hello.aop.exam;
 
+import hello.aop.exam.aop.TraceAspect;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
 /**
  * @author yjjung
@@ -11,6 +13,7 @@ import org.springframework.boot.test.context.SpringBootTest;
  * @since 2023/03/25
  */
 @Slf4j
+@Import(TraceAspect.class)
 @SpringBootTest
 class ExamTest {
 
