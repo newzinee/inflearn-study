70baf2fce0fcc96b40cb70b19f2370f4ab937686
newzinee <qvo7896@gmail.com>
2023-05-14 21:31:56 +0900

    ✨ load balancer

--- cloud-first-service/src/main/java/com/example/cloudfirstservice/WelcomeController.java
+++ cloud-first-service/src/main/java/com/example/cloudfirstservice/WelcomeController.java
@@ -1,6 +1,8 @@
 package com.example.cloudfirstservice;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/first-service")
 public class WelcomeController {
+
+    Environment env;
+
+    public WelcomeController(Environment env) {
+        this.env = env;
+    }
 
     @GetMapping("/welcome")
     public String welcome() {
@@ -23,7 +31,11 @@ public class WelcomeController {
     }
 
     @GetMapping("/check")
-    public String check() {
-        return "Hi, there. This is a message from First Service";
+    public String check(HttpServletRequest request) {
+        log.info("Server port={}", request.getServerPort());
+
+        return String.format("Hi, there. This is a message from First Service PORT: %s"
+                , env.getProperty("local.server.port"));
     }
+
 }
